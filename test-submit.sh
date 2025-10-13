eb0f3e3f985871ebcdb57197eb3b8157b52c0e04
mahmed03 <mahmoud.ahmed@icr.ac.uk>
2025-10-13 12:00:44 +0100

    update params and test setup

--- test-submit.sh
+++ test-submit.sh
@@ -7,32 +7,24 @@
 #SBATCH -t 120:00:00
 
 # Setup test directory
-mkdir -p tests/ tests/input
+mkdir -p tests/
 
-# # Download test data
-# URL="https://figshare.com/ndownloader/files"
+TESTDATA="git@github.com:genomictools/test-datasets.git"
+BRANCH="infer-cohort-ancestry"
+SRC="tests/input"
 
-# URL="https://raw.githubusercontent.com/genomictools/test-datasets/refs/heads/infer-cohort-ancestry"
-# wget -c $URL/ref.variants.vcf.gz -O tests/input/ref.variants.vcf.gz
-# wget -c $URL/ref.variants.vcf.gz.tbi -O tests/input/ref.variants.vcf.gz.tbi
-# wget -c $URL/ref.population.txt -O tests/input/ref.population.txt
-# wget -c $URL/pheno.variants.vcf.gz -O tests/input/pheno.variants.vcf.gz
-# wget -c $URL/pheno.variants.vcf.gz.tbi -O tests/input/pheno.variants.vcf.gz.tbi
-# wget -c $URL/pheno.population.txt -O tests/input/pheno.population.txt
-
-# echo -e "cohort,type,size,vars_file,vars_index,population" > tests/input/cohorts_input.csv
-# echo -e "ref,references,10,./input/ref.variants.vcf.gz,./input/ref.variants.vcf.gz.tbi,./input/ref.population.txt" >> tests/input/cohorts_input.csv
-# echo -e "pheno,cases,6,./input/pheno.variants.vcf.gz,./input/pheno.variants.vcf.gz.tbi,./input/pheno.population.txt" >> tests/input/cohorts_input.csv
+git -C $SRC pull || \
+git clone -b $BRANCH $TESTDATA $SRC
 
 # Run nextflow
 module load Nextflow
 
 cd tests/
 
-# nextflow run houlstonlab/infer-cohort-ancestry -r main \
+# nextflow run genomictools/infer-cohort-ancestry -r main \
 nextflow run ../main.nf \
     --output_dir ./results/ \
-    -profile cluster,test \
+    -profile local,test \
     -params-file ../test-params.json \
     -resume
 
